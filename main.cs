2b566ca63bd22f09bebe0476f93e4ab298d82327
JMart25 <149201983+JMart25@users.noreply.github.com>
2024-06-02 20:01:55 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class InchestoCentimeters {
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Enter the length in inches:");
+    string input =  Console.ReadLine();
+
+    if(double.TryParse(input, out double inches))
+    {
+      double centimeters = inches * 2.54;
+      Console.WriteLine($"{inches} inches is equal to {centimeters} centimeters.");
+    }
+  }
+}
